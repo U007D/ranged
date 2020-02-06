3d5fba0461c2881cd2f3126f9b8760ff16b5d13a
Brad Gibson <brad@tpfs.io>
2020-02-06 11:42:40 -0800

    RangedI32::overflowing_add(v) where v > 0: green

--- src/ranged_i32/unit_tests/overflowing_add.rs
+++ src/ranged_i32/unit_tests/overflowing_add.rs
@@ -14,7 +14,20 @@ fn overflowing_add__adding_a_non_overflowing_scalar_to_a_ranged_does_not_overflo
 }
 
 #[test]
-fn overflowing_add__adding_an_overflowing_scalar_to_a_ranged_overflows() {
+fn overflowing_add__adding_a_range_overflowing_scalar_to_a_ranged_overflows() {
+    // given
+    let ranged = RangedI32::<0, 100>::new(80).unwrap();
+    let scalar = 95;
+
+    // when
+    let res = ranged.overflowing_add(scalar);
+
+    // then
+    assert_eq!(res, (RangedI32::<0, 100>::new(75).unwrap(), true));
+}
+
+#[test]
+fn overflowing_add__adding_a_machine_word_overflowing_scalar_to_a_ranged_overflows() {
     // given
     let ranged = RangedI32::<0, 100>::new(72).unwrap();
     let scalar = i32::max_value();
